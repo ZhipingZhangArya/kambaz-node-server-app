5125b83660f86ef920c12720be6dc2be27033189
zhiping <arya.zhangpika@gmail.com>
2025-11-12 20:09:44 +0630

    Fix bugs

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ if (process.env.SERVER_ENV !== 'development') {
   sessionOptions.cookie = {
     sameSite: 'none',
     secure: true,
-    domain: process.env.SERVER_URL,
+    // For cross-origin cookies, don't set domain - browser will handle it
+    // Setting domain can cause cookies not to be sent properly
   };
 }
 
